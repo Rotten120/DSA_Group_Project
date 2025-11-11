cb0585f68dc37001694473f1e132ed2bb8b12619
Rotten120 <thenanokid@gmail.com>
2025-11-11 12:09:24 +0800

    separated each route within app.py under src/routes/ folder

---
+++ src/routes/queue_routes.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template, request
+from src.queue_deque.dataqueue import Queue
+
+queue_bp = Blueprint('queue', __name__)
+queue_out = Queue()
+
+@queue_bp.route('/init')
+def queue_init():
+    global queue_out
+    queue_out = Queue()
+    return render_template('queue.html', nodes=list(queue_out))
+
+@queue_bp.route('/update', methods=['GET', 'POST'])
+def queue_update():
+    inp = None
+    global queue_out
+    if request.method == 'POST':
+        inp = request.form.get('mainInput', '').strip()
+        action = request.form['button']
+
+        if action == 'dequeue': 
+            queue_out.dequeue()
+        elif action == 'enqueue' and inp != '':
+            queue_out.enqueue(inp)
+
+    return render_template('queue.html', nodes=list(queue_out))
+
