e54cc569cff484f616ec03af81926e5a429919d9
Rotten120 <thenanokid@gmail.com>
2025-12-06 23:39:14 +0800

    update: applied changes from bst_db.py; added bst management routes; changed api endpoints

--- src/routes/bst_route.py
+++ src/routes/bst_route.py
@@ -1,53 +1,102 @@
 from flask import Blueprint, render_template, request, jsonify
 from src.binary_search_tree.bst import BinarySearchTree
+from src.database.bst_db import BstFolderDB
 
 bst_bp = Blueprint('bst', __name__)
-bst_out = BinarySearchTree()
+bst_out = BstFolderDB('data/bst_data')
 
 @bst_bp.route('/')
 def bst_update():
     return render_template('binary-search-tree.html')
 
-@bst_bp.route('/create', methods=["GET", "POST"])
-def insert_node(value: str = None):
+@bst_bp.route('/node/create/<str: graph_name>', methods=["GET", "POST"])
+def insert_node(graph_name: str, value: str = None):
     global bst_out
 
     try:
         if not value:
-            bst_out.insert(bst.root, value)
+            graph = bst_out.get(graph_name)
+            graph.insert(graph.root, value)
     except ValueError as err:
         print(err)
-    return get_bst_json()
+    return get_bst_json(graph_name, True)
 
-@bst_bp.route('/update', methods=["GET", "PUT"])
-def update_node(old_value: str = None, new_value: str = None):
+@bst_bp.route('/node/update/<str: graph_name>', methods=["GET", "PUT"])
+def update_node(graph_name: str, old_value: str = None, new_value: str = None):
     global bst_out
 
     try:
         if not value:
-            bst_out.update(bst.root, old_value, new_value)
+            graph = bst_out.get(graph_name)
+            graph.update(graph.root, old_value, new_value)
     except ValueError as err:
         print(err)
-    return get_bst_json()
+    return get_bst_json(graph_name, True)
 
-@bst_bp.route('/delete', methods=["DELETE"])
-def delete_node(value: str):
+@bst_bp.route('/node/delete/<str: graph_name>', methods=["DELETE"])
+def delete_node(graph_name: str, value: str):
     global bst_out
-    bst_out.delete(bst.root, value)
-    return get_bst_json()
+    graph = bst_out.get(graph_name)
+    graph.delete(graph.root, value)
+    return get_bst_json(graph_name, True)
 
-@bst_bp.route('/bst/list/fetch', methods=["GET"])
-def get_inorder_traversal():
+@bst_bp.route('/graph/fetch/ordered/<str: graph_name>', methods=["GET"])
+def get_inorder_traversal(graph_name: str):
     global bst_out
-    return jsonify(bst_out.get_ordered())
+    return jsonify(bst_out.get(graph_name).get_ordered())
 
-@bst_bp.route('/bst/list/fetch', methods=["GET"])
-def get_bst_json():
+@bst_bp.route('/graph/fetch/<str: graph_name>', methods=["GET"])
+def get_bst_json(graph_name: str, is_updated: bool = False):
     global bst_out
+    graph = bst_out.get(graph_name)
+
     dict_out = {
-        "value": list(bst_out),
-        "min": bst_out.get_min(bst_out.root),
-        "max": bst_out.get_max(bst_out.root),
-        "height": bst_out.get_height(bst_out.root)
+        "value": list(graph),
+        "min": graph.get_min(graph.root),
+        "max": graph.get_max(graph.root),
+        "height": graph.get_height(graph.root)
     }
+    
+    if is_updated:
+        bst_out.upload(graph_name)
     return jsonify(dict_out)
+
+@bst_bp.route('/graph/fetch/all', methods=["GET"])
+def get_bst(is_updated: bool = False):
+    global bst_out
+    graphs = bst_out.get_all()
+    graphs_out = {}
+
+    for graph_name in graphs:
+        temp = graphs[graph_name].get()
+        graphs_out[graph_name] = {
+            "value": list(temp),
+            "min": temp.get_min(temp.root),
+            "max": temp.get_max(temp.root),
+            "height": temp.get_height(temp.root)
+        }
+
+    if is_updated:
+        bst_out.upload_all()
+    return jsonify(graphs_out)
+
+@bst_bp.route('/graph/create/<str: graph_name>', methods=["GET", "POST"])
+def insert_bst(graph_name: str):
+    global bst_out
+    
+    bst_out.insert(graph_name)
+    return get_bst(is_updated = True)
+
+@bst_bp.route('/graph/delete/<str: graph_name>', methods=["DELETE", "POST"])
+def delete_bst(graph_name: str):
+    global bst_out
+
+    bst_out.dete(graph_name)
+    return get_bst(is_updated = True)
+
+@bst_bp.route('/graph/rename/<str: old_name>', methods=["PUT", "POST"])
+def rename_bst(old_name: str, new_name: str):
+    global bst_out
+
+    bst_out.rename_file(old_name, new_name)
+    return get_bst(is_updated = True)
