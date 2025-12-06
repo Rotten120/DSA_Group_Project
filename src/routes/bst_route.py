c5b2eb7624b2c085fcecec484badfc847a8a52f6
Rotten120 <thenanokid@gmail.com>
2025-12-06 14:10:00 +0800

    add: code implementation for each endpoint

--- src/routes/bst_route.py
+++ src/routes/bst_route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 from src.binary_search_tree.bst import BinarySearchTree
 
 bst_bp = Blueprint('bst', __name__)
@@ -10,20 +10,44 @@ def bst_update():
 
 @bst_bp.route('/create', methods=["GET", "POST"])
 def insert_node(value: str = None):
-    pass
+    global bst_out
+
+    try:
+        if not value:
+            bst_out.insert(bst.root, value)
+    except ValueError as err:
+        print(err)
+    return get_bst_json()
 
 @bst_bp.route('/update', methods=["GET", "PUT"])
 def update_node(old_value: str = None, new_value: str = None):
-    pass
+    global bst_out
+
+    try:
+        if not value:
+            bst_out.update(bst.root, old_value, new_value)
+    except ValueError as err:
+        print(err)
+    return get_bst_json()
 
 @bst_bp.route('/delete', methods=["DELETE"])
 def delete_node(value: str = None):
-    pass
+    global bst_out
+    bst_out.delete(bst.root, value)
+    return get_bst_json()
 
 @bst_bp.route('/bst/list/fetch', methods=["GET"])
 def get_inorder_traversal():
-    pass
+    global bst_out
+    return jsonify(bst_out.get_ordered())
 
 @bst_bp.route('/bst/list/fetch', methods=["GET"])
-def get_bst():
-    pass
+def get_bst_json():
+    global bst_out
+    dict_out = {
+        "value": list(bst_out),
+        "min": bst_out.get_min(bst_out.root),
+        "max": bst_out.get_max(bst_out.root),
+        "height": bst_out.get_height(bst_out.root)
+    }
+    return jsonify(dict_out)
