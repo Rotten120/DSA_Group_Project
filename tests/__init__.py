fddb432054f35d40bd7749b741fc5747596baf5c
Rotten120 <thenanokid@gmail.com>
2025-12-16 17:46:43 +0800

    add: imports of other testcases in tests/__init__.py so that all tests can be executed at once

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+import unittest
+
+from .test_bitree import TestBinaryTree 
+from .test_deque import TestDeQue
+from .test_queue import TestQueue
+
+
+if __name__ == "__main__":
+    unittest.main()
