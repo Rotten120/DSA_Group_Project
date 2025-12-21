deff781f3d8a07d2ce2ae9d2fcd2ca7ff2023d7b
Rotten120 <thenanokid@gmail.com>
2025-12-21 21:19:24 +0800

    add: graph main and search routes

---
+++ src/routes/graph_route.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template, jsonify
+from src.logic.graph_bfs import Graph, Node
+from database.train_db import TrainDB
+import json
+
+graph_bp = Blueprint('graph', __name__)
+folder_path = "data/railway_fare/"
+graph_out = TrainDB(folder_path, fetch_data = True)
+
+@graph_bp.route('/')
+def graph_update():
+    global graph_bp
+    return render_template('directions.html')
+
+@graph_bp.route('/search/path/<string:start>/<string:end>')
+def search_shortest_path_by_stations(start: str, end: str):
+    global graph_bp
+    return jsonify(graph_bp.get().bfs(start, end))
