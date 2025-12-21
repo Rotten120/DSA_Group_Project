584b8cc451ae0fe233bf69ca8ad5af47fd9a70c0
Rotten120 <thenanokid@gmail.com>
2025-12-21 11:13:47 +0800

    refactor: api is separated to details and resources (images)

--- src/routes/project_route.py
+++ src/routes/project_route.py
@@ -1,17 +1,32 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, send_from_directory
 
 projects_bp = Blueprint('projects', __name__)
 filepath = "data/project_details.json"
 project_details = None
 
+def load_project_details():
+    if not project_details is None:
+        return
+
+    with open(filepath, 'r') as file:
+        projects = json.load(file)
+
 @projects_bp.route('/')
 def projects():
     return render_template('project.html')
 
-@projects_bp.route('/fetch/all', methods=["GET"])
-def fetch_project_details():
-    if project_details is None:
-        with open(filepath, 'r') as file:
-            project_details = json.load(file)
+@projects_bp.route('/fetch/desc/<string:title>', methods=["GET"])
+def fetch_project_details(title: str):
+    load_project_details()
 
-    return jsonify(project_details)
+    if title in project_details:
+        return jsonify(project_details[title])
+    return jsonify(project_details["template_"])
+
+@projects_bp.route('/fetch/image/<string:title>')
+def fetch_project_image(title: str):
+    load_project_details()
+
+    if title in project_details:
+        return send_from_directory('data', project_details[title]["bg-img"])
+    return send_from_directory('data', project_details["template_"]["bg-img"])
