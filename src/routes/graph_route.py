60f324ea8a8ed8e54b7e56a19b45e95cca46eb95
Rotten120 <thenanokid@gmail.com>
2025-12-23 10:43:00 +0800

    update: how parse_bfs parses the path and tag params (weight was also removed)

--- src/routes/graph_route.py
+++ src/routes/graph_route.py
@@ -7,22 +7,37 @@ graph_bp = Blueprint('graph_', __name__)
 folder_path = "data/railway_fare"
 graph_out = TrainDB(folder_path, fetch_data = True)
 
-graph_out.get().print()
-
-def parse_bfs(path: list[str], weight: TrainWeight, tags: list[str]):
-    parsed_dict = {
-        "path": [],
-        "stored value": weight.stored,
-        "single journey": weight.single,
-        "time": weight.time
-    }
-
+def parse_bfs(path: list[str], tags: list[str]):
+    parsed_path = []
+    parsed_weight = TrainWeight()
+    from_station = path[0]
     temp_arr = [tags[0]]
+  
+    def update_weights(from_s, to_s, train = None):
+        nonlocal parsed_weight
+        nonlocal parsed_path
+        nonlocal temp_arr
+
+        parsed_weight += graph_out.get_weights(from_s, to_s)
+        parsed_path.append(temp_arr)
+        temp_arr = [train]
+
     for p, t in zip(path, tags):
         if t != temp_arr[0]:
-            parsed_dict["path"].append(temp_arr)
-            temp_arr = [t]
+            #adds the weight when transferring trains
+            parsed_weight += graph_out.get_weights(temp_arr[-1], p)
+
+            update_weights(from_station, temp_arr[-1], t)
+            from_station = p
         temp_arr.append(p)
+    update_weights(from_station, path[-1])
+
+    parsed_dict = {
+        "path": parsed_path,
+        "stored value": parsed_weight.stored,
+        "single journey": parsed_weight.single,
+        "time": parsed_weight.time
+    }
     return parsed_dict
      
 @graph_bp.route('/')
@@ -32,10 +47,9 @@ def graph_update():
 @graph_bp.route('/search/path/<string:start>/<string:end>')
 def search_shortest_path_by_stations(start: str, end: str):
     try:
-        path, weight, tags = graph_out.get().bfs(
+        path, tags = graph_out.get_graph().bfs(
             start,
             end,
-            initial_weight = TrainWeight(),
             include_tags = True
         )
 
@@ -45,9 +59,10 @@ def search_shortest_path_by_stations(start: str, end: str):
         if len(path) == 0:
             return jsonify(message="No valid route found"), 404
     
-        return jsonify({parse_bfs(path, weight, tags)}), 200
+        return jsonify(parse_bfs(path, tags)), 200
         
     except Exception as e:
+        raise Exception(e)
         return jsonify(message=f"Error processing route: {str(e)}"), 500
     
 @graph_bp.route('/search/time/<string:start>/<string:end>')
