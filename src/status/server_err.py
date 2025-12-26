c3c280f6f5ef542b969491b62ea55cff6ed8cbb4
Rotten120 <thenanokid@gmail.com>
2025-12-26 14:10:33 +0800

    add: ServerError.response which returns a jsonified copy of the exception

--- src/status/server_err.py
+++ src/status/server_err.py
@@ -1,10 +1,25 @@
-from .code import StatusCodes, Status
+from flask import jsonify
+from .code import StatusCodes
 
 class ServerError(Exception):
     def __init__(
             self,
-            status: Status = StatusCodes.INTERNAL_SERVER_ERROR,
+            status: StatusCodes = StatusCodes.INTERNAL_SERVER_ERR,
             description: str | None = None
     ):
-        super().__init__(status.message(description))
+        self.message = status.message(description)
         self.code = status.code
+        super().__init__(self.message)
+
+    def __handle(self, **kwargs):
+        return jsonify(
+            message = self.message,
+            **kwargs
+        ), self.code
+
+    @classmethod
+    def response(cls, err: Exception, **kwargs):
+        if isinstance(err, cls):
+            return err.__handle(**kwargs)
+        temp_err = ServerError(description = str(err))
+        return temp_err.__handle(**kwargs)
