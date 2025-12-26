fd6d9f52fe64208c3b3d47315b7b8b67d2a96e08
Rotten120 <thenanokid@gmail.com>
2025-12-26 14:13:16 +0800

    refactor: functions and attrs of Status is combined with StatusCodes instead (function of Enums)

--- src/status/code.py
+++ src/status/code.py
@@ -1,19 +1,18 @@
-from typing import Enum
+from enum import Enum
 
-class Status:
-    def __init__(self, title: str, code: int)
+class StatusCodes(Enum):
+    SUCCESS =               ("Request Success", 200)
+    CREATED =               ("Entry Created", 201)
+    BAD_REQUEST =           ("Bad Request", 400)
+    NOT_FOUND =             ("Not Found", 404)
+    INTERNAL_SERVER_ERR =   ("Server Error", 500)
+
+    def __init__(self, title: str, code: int):
         self.title = title
         self.code = code
 
     def message(self, description: str | None = None):
         msg_out = f"{self.code} {self.title}"
-        if not description:
+        if description:
             msg_out += f": {description}"
         return msg_out
-
-class StatusCodes(Enum):
-    SUCCESS =               Status("Request Success", 200)
-    CREATED =               Status("Entry Created", 201)
-    BAD_REQUEST =           Status("Bad Request", 400)
-    NOT_FOUND =             Status("Not Found", 404)
-    INTERNAL_SERVER_ERR =   Status("Server Error", 500)
