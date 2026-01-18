df382ca814f382b5c2eef9815887140ec408d34d
Rotten120 <thenanokid@gmail.com>
2026-01-18 23:36:52 +0800

    Update: tests/__init__.py to also add the new test cases

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,18 @@
 import unittest
 
+from .test_binary_search_tree import TestBSTInsert, TestBSTSearch, TestBSTDelete, TestBSTUpdate, TestBSTProperties
+from .test_graph import TestNode, TestGraph
+
 from .test_bitree import TestBinaryTree 
 from .test_deque import TestDeQue
 from .test_queue import TestQueue
 
+
+from .test_bubble_sort import TestBubbleSort
+from .test_insertion_sort import TestInsertionSort
+from .test_merge_sort import TestMergeSort
+from .test_quick_sort import TestQuickSort
+from .test_selection_sort import TestSelectionSort
 
 if __name__ == "__main__":
     unittest.main()
