7d4f36532778c2d364c9b0cfb3c8ce1272a8499a
Rotten120 <thenanokid@gmail.com>
2026-01-18 14:23:44 +0800

    Add: updated sorting algorithms

--- src/logic/sorting_algos/selection_sort.py
+++ src/logic/sorting_algos/selection_sort.py
@@ -2,8 +2,9 @@ def selection_sort(arr, *args):
     n = len(arr)
     for i in range(n - 1):
         min_index = i
-        for j in range(i, n):
+        for j in range(i + 1, n):
             yield arr, j, -1, i, -1
             if arr[j] < arr[min_index]:
                 min_index = j
         arr[i], arr[min_index] = arr[min_index], arr[i]
+    yield arr, -1, -1, -1, -1
