5818829997d0b2158f18c1bd40c73a92741213a3
Shane <palomoshanecraven@gmail.com>
2026-01-18 14:55:49 +0800

    Add: recover code

--- src/logic/sorting_algos/selection_sort.py
+++ src/logic/sorting_algos/selection_sort.py
@@ -2,7 +2,7 @@ def selection_sort(arr, *args):
     n = len(arr)
     for i in range(n - 1):
         min_index = i
-        for j in range(i + 1, n):
+        for j in range(i, n):
             yield arr, j, -1, i, -1
             if arr[j] < arr[min_index]:
                 min_index = j
