45298a91ea56c24a2cf1383c26f8ff364d55bb2f
Shane <palomoshanecraven@gmail.com>
2026-01-18 11:20:16 +0800

    Add: fix button and add step title for sorting algo. Then, merge pop-up in projects section.

--- static/js/side_bar.js
+++ static/js/side_bar.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuIcon = document.getElementById("menu-icon");
     const sidebar = document.getElementById("sidebar");
-    const overlay = document.getElementById("overlay");
+    const sidebarOverlay = document.getElementById("sidebar-overlay");
 
     function toggleSidebar() {
         sidebar.classList.toggle("active");
-        overlay.classList.toggle("active");
+        sidebarOverlay.classList.toggle("active");
     }
 
-    menuIcon.addEventListener("click", toggleSidebar);
-    overlay.addEventListener("click", toggleSidebar);
+    if (menuIcon) {
+        menuIcon.addEventListener("click", toggleSidebar);
+    }
+    
+    if (sidebarOverlay) {
+        sidebarOverlay.addEventListener("click", toggleSidebar);
+    }
 });
